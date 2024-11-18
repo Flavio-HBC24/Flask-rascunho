c4ace13dcbcdd04809b28142287203116e34e04d
Flavio-HBC24 <161040937+Flavio-HBC24@users.noreply.github.com>
2024-11-18 17:19:51 -0300

    Add files via upload

---
+++ aaaaaatestte/aulas_3bim/models.py
@@ -0,0 +1,35 @@
+from database import db
+
+
+class Empresa(db.Model):
+    __tablename__='empresa'
+    id_empresa = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(100))
+    cidade = db.Column(db.String(100))
+    
+
+    def __init__(self, nome, cidade):
+        self.nome = nome
+        self.cidade = cidade
+    
+    def __repr__(self):
+        return "<Empresa {}>".format(self.nome)
+    
+class Funcionarios(db.Model):
+    __tablename__='funcionarios'
+    id_funcionario = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(100))
+    cargo = db.Column(db.String(100))
+    id_empresa = db.Column(db.Integer, db.ForeignKey('id_empresa'))
+    
+
+    def __init__(self, nome, cargo, id_empresa):
+        self.nome = nome
+        self.cargo = cargo
+        self.id_empresa = id_empresa
+
+    id_empresa = db.relationship('Empresa', foreign_keys=id_empresa)
+    
+    def __repr__(self):
+        return "<Empresa {}>".format(self.nome)
+    
